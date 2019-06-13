d9bca6978e7d074c037f0697f9ce2b3dfdcfc832
benmfoster <bfoster@marlboro.edu>
2019-06-13 10:46:14 -0400

    adds javascript front end

--- app/controllers/api/actor_controller.rb
+++ app/controllers/api/actor_controller.rb
@@ -16,10 +16,10 @@ class Api::ActorController < ApplicationController
     	@actor = Actor.new(
     		first_name: params[:first_name],
     		last_name: params[:last_name],
-			known_for: params[:known_for],
-			gender: params[:gender],
-			age: params[:age],
-			movie_id: params[:movie_id]
+				known_for: params[:known_for],
+				gender: params[:gender],
+				age: params[:age],
+				movie_id: params[:movie_id]
 			)
 			
 			if @actor.save
@@ -36,10 +36,10 @@ class Api::ActorController < ApplicationController
 
     	@actor.first_name = params[:first_name] || @actor.first_name
     	@actor.last_name = params[:last_name] || @actor.last_name
-		@actor.known_for = params[:known_for] || @actor.known_for
-		@actor.gender = params[:gender] || @actor.gender
-		@actor.age = params[:age] || @actor.age
-		@actor.movie_id = params[:movie_id] || @actor.movie_id
+			@actor.known_for = params[:known_for] || @actor.known_for
+			@actor.gender = params[:gender] || @actor.gender
+			@actor.age = params[:age] || @actor.age
+			@actor.movie_id = params[:movie_id] || @actor.movie_id
 
     	@actor.save
     	render 'show.json.jbuilder'
